0bd5e9831b820ef168c4997caad157cc57dc22a7
Quân Nguyễn <quanlh3052004@gmail.com>
2026-06-20 21:17:07 +0700

    start project

---
+++ src/main/java/kuan/fintech/fintech_kyc_service/FintechKycServiceApplication.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_kyc_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechKycServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechKycServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kuan/fintech/fintech_kyc_service/FintechKycServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kuan.fintech.fintech_kyc_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechKycServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
